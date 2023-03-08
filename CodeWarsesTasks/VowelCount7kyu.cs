73489e89a06bfd126ff89140d94f43bc047b7747
hempi <115639626+VladislavBukirev@users.noreply.github.com>
2023-03-08 21:02:42 +0500

    Initial commit

---
+++ CodeWarsesTasks/VowelCount7kyu.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+using NUnit.Framework;
+
+public static class Kata
+{
+    public static int GetVowelCount(string str)
+    {
+        return str.Count(c => "aeiou".Contains(c));
+    }
+}
+
+[TestFixture]
+public class KataTests
+{
+    [Test]
+    public void TestCase1()
+    {
+        Assert.AreEqual(5, Kata.GetVowelCount("abracadabra"), "Nope!");
+    }
+}
